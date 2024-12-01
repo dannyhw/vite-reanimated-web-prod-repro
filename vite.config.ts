e553c0f242b4c2e1e4ec40937dcc64ed10827ab7
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-12-01 11:55:57 +0000

    stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,56 @@
 // https://stereobooster.com/posts/react-native-web-with-vite/
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 const extensions = [
-  '.web.tsx',
-  '.tsx',
-  '.web.ts',
-  '.ts',
-  '.web.jsx',
-  '.jsx',
-  '.web.js',
-  '.js',
-  '.css',
-  '.json',
-  '.mjs',
+  ".web.tsx",
+  ".tsx",
+  ".web.ts",
+  ".ts",
+  ".web.jsx",
+  ".jsx",
+  ".web.js",
+  ".js",
+  ".css",
+  ".json",
+  ".mjs",
 ];
 
-const development = process.env.NODE_ENV === 'development';
+const development = process.env.NODE_ENV === "development";
 
 export default defineConfig({
   clearScreen: true,
   plugins: [
     react({
       babel: {
-        // presets: ['babel-preset-expo'],
         plugins: [
-          '@babel/plugin-proposal-export-namespace-from',
-          'react-native-reanimated/plugin',
+          "react-native-web",
+          "@babel/plugin-proposal-export-namespace-from",
+          "react-native-reanimated/plugin",
         ],
-        configFile: true,
       },
     }),
   ],
   define: {
-    __DEV__: JSON.stringify(development),
-    DEV: JSON.stringify(development),
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    'global.__x': {},
+    __DEV__: development,
+    DEV: development,
+    "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`,
+    "global.__x": {},
     _frameTimestamp: undefined,
     _WORKLET: false,
   },
   resolve: {
     extensions,
     alias: {
-      'react-native': 'react-native-web',
+      "react-native": "react-native-web",
     },
+    mainFields: ["browser", "module", "main", "react-native"],
   },
   optimizeDeps: {
     esbuildOptions: {
       resolveExtensions: extensions,
-      jsx: 'automatic',
-      loader: { '.js': 'jsx' },
+      jsx: "automatic",
+      loader: { ".js": "jsx" },
     },
   },
 });
